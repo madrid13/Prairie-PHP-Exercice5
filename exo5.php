750454c23a8406880362e1ec13ea4374f1031e79
madrid13 <madridmaxime13@gmail.com>
2016-05-10 12:15:07 +0200

    exo5.php

---
+++ exo5.php
@@ -0,0 +1,8 @@
+<?php
+$chaine1 = "Pour jouer au rugby,";
+$chaine2 = " Il faut être passionné";
+function fusion_chaine ($chaine1, $chaine2) {
+  return $chaine1.$chaine2;
+}
+echo fusion_chaine($chaine1,$chaine2);
+?>
